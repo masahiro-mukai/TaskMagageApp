979275f6a25bca85a97877176f5e83bcaa44087d
masahiro.mukai <mm1006@p1.tcnet.ne.jp>
2020-01-25 09:16:31 +0900

    Lesson6 タスク管理アプリ 課題修正前

--- TaskManageApp/InputViewController.swift
+++ TaskManageApp/InputViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import RealmSwift
+import UserNotifications
 
 class InputViewController: UIViewController {
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
+
+    let realm = try! Realm()
+    var task: Task!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,17 +26,73 @@ class InputViewController: UIViewController {
         self.contentsTextView.layer.borderWidth = 1.0
         self.contentsTextView.layer.cornerRadius = 10
         self.contentsTextView.layer.masksToBounds = true
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する。
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
     }
     
+    // DB更新(遷移する際、画面が非表示になる時に呼ばれるメソッド)
+     override func viewWillDisappear(_ animated: Bool) {
+         try! realm.write {
+             self.task.title = self.titleTextField.text!
+             self.task.contents = self.contentsTextView.text
+             self.task.date = self.datePicker.date
+             self.realm.add(self.task, update: .modified)
+         }
+         
+         setNotification(task: task)
+         super.viewWillDisappear(animated)
+     }
+     
+     // タスクのローカル通知を登録する --- ここから ---
+     func setNotification(task: Task) {
+         let content = UNMutableNotificationContent()
+         // タイトルと内容を設定(中身がない場合メッセージ無しで音だけの通知になるので「(xxなし)」を表示する)
+         if task.title == "" {
+             content.title = "(タイトルなし)"
+         } else {
+             content.title = task.title
+         }
+         if task.contents == "" {
+             content.body = "(内容なし)"
+         } else {
+             content.body = task.contents
+         }
+         content.sound = UNNotificationSound.default
 
-    /*
-    // MARK: - Navigation
+         // ローカル通知が発動するtrigger（日付マッチ）を作成
+         let calendar = Calendar.current
+         let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
+         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+         // identifier, content, triggerからローカル通知を作成（identifierが同じだとローカル通知を上書き保存）
+         let request = UNNotificationRequest(identifier: String(task.id), content: content, trigger: trigger)
+
+         // ローカル通知を登録
+         let center = UNUserNotificationCenter.current()
+         center.add(request) { (error) in
+             print(error ?? "ローカル通知登録 OK")  // error が nil ならローカル通知の登録に成功したと表示します。errorが存在すればerrorを表示します。
+         }
+
+         // 未通知のローカル通知一覧をログ出力
+         center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+             for request in requests {
+                 print("/---------------")
+                 print(request)
+                 print("---------------/")
+             }
+         }
+     } // --- ここまで追加 ---
+
+     @objc func dismissKeyboard() {
+         // キーボードを閉じる
+         view.endEditing(true)
+     }
 
 }
